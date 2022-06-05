8cd28622cbcbf8e41d8201e2773b3790a9434c34
anthony luque <kasuru@hotmail.com>
2022-06-04 20:08:10 -0500

    [ADD] error handler, prettier and eslit config

--- app/utils/ErrorHandler.js
+++ app/utils/ErrorHandler.js
@@ -1,7 +1,20 @@
 class ErrorHandler {
   constructor() {}
 
-  static GraphQlError(error, message, functinName, fileName, moduleName) {
-    console.error();
+  static GraphQlError({
+    error,
+    message,
+    functionName,
+    fileName,
+    moduleName,
+    res,
+  }) {
+    console.error(
+      `Error ${message} [${functionName} - ${fileName} - ${moduleName}] | ${error}`
+    );
+
+    if (res) {
+      res.status(500).json(`Error ${message}`);
+    }
   }
 }
